706321c1108ae7e4b64ce762f738a5796ae5f5df
Alexandra Leonova <kabrenok@gmail.com>
2017-12-16 21:29:05 +0300

    Разбивает на модули

---
+++ js/util.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function () {
+  var ESC_KEY = 27;
+  var ENTER_KEY = 13;
+
+  window.util = {
+    isEscEvent: function (evt, action) {
+      if (evt.keyCode === ESC_KEY) {
+        action();
+      }
+    },
+    isEnterEvent: function (evt, action) {
+      if (evt.keyCode === ENTER_KEY) {
+        action();
+      }
+    },
+    getRandomNumber: function (min, max) {
+      return Math.floor(min + Math.random() * (max + 1 - min));
+    }
+  };
+})();
+
+
